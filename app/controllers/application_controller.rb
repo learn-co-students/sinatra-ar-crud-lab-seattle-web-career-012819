f20d85fe4d0cc8ac0a76cb331f0d93c9a27015b5
Lucy Suddenly <lucysuddenly@Lucys-Air.local.tld>
2019-02-21 07:57:57 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,42 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  post '/articles' do
+    a = Article.create(params)
+    redirect "/articles/#{a.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @editArticle = Article.find(params[:id])
+    my_params = params.reject {|k,v| k == "_method"}
+    @editArticle.update(my_params)
+    redirect "/articles/#{@editArticle.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect '/articles'
+  end
+
 end
